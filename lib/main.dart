addc611b5ad73620ddefc62fc062a8286f1641e2
Bijay Yadav <bijay.yadav@cloudyfox.com>
2019-07-18 17:10:12 +0545

    Generate QR Code for the particular text and also provide the size of the QR Code.

--- lib/main.dart
+++ lib/main.dart
@@ -22,24 +22,95 @@ class StateFul extends State<MyApp> {
 
 class HomePage extends StatelessWidget {
   TextEditingController textController = TextEditingController();
-  double finalSize = 0;
+  TextEditingController sizeController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    double finalSize = size.width;
+    return Container(child: getForm(context));
+  }
 
-    if (size.height <= size.width) {
-      finalSize = size.height;
-    }
+  Widget getForm(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR Code Generator"),
+        leading: IconButton(
+          icon: Icon(Icons.movie),
+          onPressed: null,
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              keyboardType: TextInputType.text,
+              textDirection: TextDirection.ltr,
+              controller: textController,
+              decoration: InputDecoration(
+                  labelText: "Enter Text",
+                  hintText: "Enter text to generate QR Code",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0))),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              keyboardType: TextInputType.text,
+              textDirection: TextDirection.ltr,
+              controller: sizeController,
+              decoration: InputDecoration(
+                  labelText: "Enter QR Code Size",
+                  hintText: "Enter Size of the QR Code",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0))),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              child: Text("Generate QR Code"),
+              onPressed: () {
+                String text = textController.text;
+                String size = sizeController.text;
 
-    debugPrint("Size: $finalSize");
+                if (text.isNotEmpty && size.isNotEmpty) {
+                  double finalSize = double.parse(size);
+                  if (finalSize < 150 || finalSize > 700) {
+                    finalSize = 300;
+                  }
 
-    return Container(
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    debugPrint("Size: $finalSize, text: $text");
+                    return QRCodeImage(finalSize, text);
+                  }));
+                }
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class QRCodeImage extends StatelessWidget {
+  double finalSize = 0;
+  String text = "QR Code";
+
+  QRCodeImage(this.finalSize, this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
         child: Scaffold(
+            appBar: AppBar(
+              title: Text(text),
+            ),
             body: new QrImage(
-      data: "1234567890",
-      size: finalSize,
-    )));
+              data: text,
+              size: finalSize,
+            )));
+    ;
   }
 }
